9ca3839e55931a60781dc67e18679d10201bb71f
Nabil Salah <nabil.salah203@gmail.com>
2024-07-02 16:14:53 +0300

    feat: adding gin http project implementation adding docker compose that run both images

--- datetimegin/cmd/main.go
+++ datetimegin/cmd/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	date_time "github.com/codescalersinternships/datetime-server-nabil/datetimegin/api/handler"
 	"github.com/fvbock/endless"
@@ -10,9 +11,14 @@ import (
 func main() {
 	r := date_time.SetupRouter()
 	// Listen and Server in 0.0.0.0:8090
-	r.Run(":8090")
-	endless.ListenAndServe(":8090", r)
-
+	err := r.Run(":8090")
+	if err != nil {
+		log.Fatal("can't run server correctly", err)
+	}
+	err = endless.ListenAndServe(":8090", r)
+	if err != nil {
+		log.Fatal("can't end server gracefull", err)
+	}
 	fmt.Println("\nGracefully shutting down HTTP server...")
 	fmt.Println("Shutdown complete.")
 }
