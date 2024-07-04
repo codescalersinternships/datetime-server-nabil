f12b4d7cdcd7a2de2d966755ec7e172f031897f1
Nabil Salah <nabil.salah203@gmail.com>
2024-07-05 01:55:19 +0300

    feat: readme is added and used make in project

--- api/datetimehttphandler/datetime.go
+++ api/datetimehttphandler/datetime.go
@@ -6,6 +6,12 @@ import (
 	"time"
 )
 
+// GetDateAndTime takes in a response writer and a request
+// and respond the current date and time in the format
+// "2006-01-02 15:04:05" to the response writer.
+// If the request is valid, it responds with a 200 OK status and the current date and time.
+// If the request method is not GET, it responds with a 405 Method Not Allowed status.
+// If the request URL is not "/datetime", it responds with a 404 Not Found status.
 func GetDate(w http.ResponseWriter, req *http.Request) {
 	if req.URL.Path != "/datetime" {
 		http.NotFound(w, req)
@@ -15,6 +21,6 @@ func GetDate(w http.ResponseWriter, req *http.Request) {
 		w.WriteHeader(http.StatusMethodNotAllowed)
 		return
 	}
-	currentDateTime := time.Now().Format(time.RFC3339)
+	currentDateTime := time.Now().Format("2003-29-05 01:02:00")
 	fmt.Fprint(w, currentDateTime)
 }
